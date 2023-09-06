f82e80d71819918552ce88b7ae1057b51f9c5bec
Jamey Smith <smith.jameyd@gmail.com>
2023-09-05 19:29:30 -0500

    refactor: update contact links

--- src/data/contactLinks.ts
+++ src/data/contactLinks.ts
@@ -1,18 +1,18 @@
 export const contactLinks = [
   {
-    href: "/contact",
-    label: "Email",
+    href: '/contact',
+    label: 'Email',
+  },
+  // {
+  //   href: "https://www.etsy.com/shop/CliffandSky",
+  //   label: "Etsy Shop",
+  // },
+  {
+    href: 'https://www.instagram.com/cliffandsky',
+    label: 'Instagram',
   },
   {
-    href: "https://www.etsy.com/shop/CliffandSky",
-    label: "Etsy Shop",
-  },
-  {
-    href: "https://www.instagram.com/cliffandsky",
-    label: "Instagram",
-  },
-  {
-    href: "https://www.pinterest.com/cdevictoriam",
-    label: "Pinterest",
+    href: 'https://www.pinterest.com/cdevictoriam',
+    label: 'Pinterest',
   },
 ];
