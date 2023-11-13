6b799b2a7d390057e54626c2bec92161701fb83c
Jamey Smith <smith.jameyd@gmail.com>
2023-11-13 13:20:30 +0100

    build: Update package and image configurations

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "cdn.sanity.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+        pathname: "/images/**",
+      },
+    ],
   },
 };
 
